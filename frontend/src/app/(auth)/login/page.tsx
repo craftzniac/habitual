311cae988fb10f1f9f129d16ae616b14dad305c0
MikeyOnyedika <michaelvik520@gmail.com>
2024-07-10 13:12:21 +0100

    minor change to auth page UIs

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,21 +1,29 @@
+"use client"
 import Image from "next/image";
 import Logo from "@/app/assets/logo.svg"
 import Link from "next/link"
 import TextField from "@/app/components/form/TextField";
 import Button from "@/app/components/form/Button";
 import { GoogleLogo } from "@/app/assets/icons";
+import { ChangeEvent, FormEvent } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+    const router = useRouter()
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        router.push("/overview")
+    }
     return (
         <div className="w-full h-full flex p-4">
             <main className="w-full h-full flex flex-col items-center gap-4">
                 <Image src={Logo} alt="logo" />
                 <div className="flex flex-col w-full h-full items-center gap-4">
                     <h1 className="font-bold text-lg">Welcome Back!</h1>
-                    <form className="w-full flex flex-col gap-4">
+                    <form className="w-full flex flex-col gap-4" onSubmit={handleSubmit}>
                         <TextField label="Email" name="email" />
                         <TextField label="Password" name="password" />
-                        <Button label="Login" />
+                        <Button label="Login" isSubmit={true} />
                     </form>
                     <p className="text-[0.81rem] font-bold text-gray-75">Or Continue with</p>
                     <Button variant="gray" label="Google">
--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -1,25 +1,33 @@
+"use client"
 import Image from "next/image";
 import Logo from "@/app/assets/logo.svg"
 import Link from "next/link"
 import TextField from "@/app/components/form/TextField";
 import Button from "@/app/components/form/Button";
 import { GoogleLogo } from "@/app/assets/icons";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
 
 export default function Signup() {
+  const router = useRouter()
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    router.push("/overview")
+  }
   return (
     <div className="w-full h-full flex p-4">
       <main className="w-full h-full flex flex-col items-center gap-4">
         <Image src={Logo} alt="logo" />
         <div className="flex flex-col w-full h-full items-center gap-4">
           <h1 className="font-bold text-lg">Create Account</h1>
-          <form className="w-full flex flex-col gap-4">
+          <form className="w-full flex flex-col gap-4" onSubmit={handleSubmit}>
             <TextField label="Username" name="username" />
             <TextField label="Email" name="email" />
             <TextField label="Password" name="password" />
-            <Button label="Login" />
+            <Button label="Signup" isSubmit={true} />
           </form>
           <p className="text-[0.81rem] font-bold text-gray-75">Or Continue with</p>
-          <Button variant="gray" label="Google">
+          <Button variant="gray" label="Google" isSubmit={true}>
             <Image src={GoogleLogo} alt="google logo" />
           </Button>
 
