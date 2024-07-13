7e1e9593e6fe43e8d5723c6f75188fad9e7c4fcf
MikeyOnyedika <michaelvik520@gmail.com>
2024-07-13 15:42:11 +0100

    minor changes to TextField and TextArea components

--- frontend/src/app/components/form/TextArea.tsx
+++ frontend/src/app/components/form/TextArea.tsx
@@ -1,14 +1,15 @@
 type Props = {
     label: string,
     name: string,
-    errMsg?: string
+    errMsg?: string,
+    placeholder?: string
 }
 
-export default function TextArea({ label, name, errMsg }: Props) {
+export default function TextArea({ placeholder, label, name, errMsg }: Props) {
     return (
         <div className="flex flex-col w-full gap-1">
             <label htmlFor={name} className="text-[0.81rem] font-bold">{label}</label>
-            <textarea id={name} className="border-[1px] text-[0.81rem] rounded-lg p-3 h-24 border-primary-300 w-full"></textarea>
+            <textarea id={name} placeholder={placeholder || ""} className="border-2 text-[0.81rem] rounded-lg p-3 h-24 border-primary-100 w-full"></textarea>
             {
                 errMsg && (
                     <p>{errMsg}</p>
--- frontend/src/app/components/form/TextField.tsx
+++ frontend/src/app/components/form/TextField.tsx
@@ -2,20 +2,21 @@ type Props = {
     label: string,
     name: string,
     type?: "password" | "text",
-    errMsg?: string,
+    infoText?: string,
+    infoTextType?: "error" | "info"
     placeholder?: string
 }
 
-export default function TextField({ label, name, type = "text", errMsg, placeholder }: Props) {
+export default function TextField({ label, name, type = "text", infoText, infoTextType = "error", placeholder }: Props) {
     return (
         <div className="flex flex-col w-full gap-1">
             <label htmlFor={name} className="text-[0.81rem] font-bold">{label}</label>
             <input placeholder={placeholder || ""} id={name} type={type} className="border-2 text-[0.81rem] rounded-lg p-3 border-primary-100 w-full" />
             {
-                errMsg && (
-                    <p>{errMsg}</p>
+                infoText && (
+                    <p className={`text-xs ${infoTextType === "error" ? "text-red" : "text-primary-500"}`}>{infoText}</p>
                 )
             }
-        </div>
+        </div >
     )
 }
