a11c21d4c34e22154e096adbb2ed41e8ee276ae3
MikeyOnyedika <michaelvik520@gmail.com>
2024-09-13 13:11:01 +0100

    Restore useIsDesktop.ts file

---
+++ frontend/src/app/hooks/useIsDesktop.ts
@@ -0,0 +1,22 @@
+"use client"
+import { useEffect, useState } from "react"
+
+export function useIsDesktop() {
+	const mdScreen = 768;
+	const [isDesktop, setIsDesktop] = useState(window.innerWidth >= mdScreen)
+	useEffect(() => {
+		function handleResize() {
+			setIsDesktop(window.innerWidth >= mdScreen)
+		}
+
+		window.addEventListener("resize", handleResize)
+
+		return () => {
+			window.removeEventListener("resize", handleResize)
+		}
+	}, [])
+
+	return {
+		isDesktop
+	}
+}
