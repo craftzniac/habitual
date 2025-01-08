fa683bd6467c0b153cca24ebc7b5cd9ac5140b66
craftzniac <76903819+craftzniac@users.noreply.github.com>
2025-01-08 12:15:17 +0100

    feat: add logout

--- frontend/src/app/(pages)/(main)/components/presentation/LogoutBtn.tsx
+++ frontend/src/app/(pages)/(main)/components/presentation/LogoutBtn.tsx
@@ -1,9 +1,10 @@
 import { Exit_16 } from "@/app/assets/icons"
+import { signOut } from "next-auth/react";
 import Image from "next/image";
 
 export default function LogoutBtn() {
     return (
-        <button className="flex items-center gap-2 justify-start px-6 py-3 w-full rounded hover:bg-primary-50 transition-colors duration-200">
+        <button className="flex items-center gap-2 justify-start px-3 py-3 w-full rounded hover:bg-primary-50 transition-colors duration-200" onClick={() => signOut()}>
             <Image src={Exit_16} alt="" className="w-6 h-6" width={16} height={16} />
             <span className="hidden 3xl:block font-bold text-sm text-primary-500">Logout</span>
         </button>
