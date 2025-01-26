40f3b738fc83786f332ce26b98c361120e066c58
MikeyOnyedika <michaelvik520@gmail.com>
2025-01-26 15:13:06 +0100

    console.log debug for env var

--- backend/src/db/datasource.ts
+++ backend/src/db/datasource.ts
@@ -2,8 +2,8 @@ import { config } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 config();
 
-const typeormConfig: Partial<DataSourceOptions> =
-  process.env.NODE_ENV === 'development'
+function getTypeormConfig() {
+  return process.env.NODE_ENV === 'development'
     ? {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
@@ -13,10 +13,10 @@ const typeormConfig: Partial<DataSourceOptions> =
     : {
         url: process.env.DB_URL,
       };
+}
 
-console.log('config: ', typeormConfig);
 export const dataSourceOptions: DataSourceOptions = {
-  ...typeormConfig,
+  ...getTypeormConfig(),
   type: 'postgres' as const,
   useUTC: true,
 };
